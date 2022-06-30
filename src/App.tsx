83e0280940fc070285bfbccfa4f572c3519b4cce
iltimas doha <iltimas.doha@gmail.com>
2022-06-30 18:07:28 -0400

    Revert "Reduce OOP logic"
    This reverts commit 8b876e71683418dbae3aec89a257d24f5d651591.

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,51 @@
-// import React from "react";
-import "./App.css";
-import HeaderSection from "./sections/HeaderSection";
-//this is a lot of importing, check out how other code bases group there exports
-//the goal would be to have a line that looks like:
-// import {experienceSection,educationSection,skillsSection, etc} from "./sections/index.tsx"
-import experienceSection from "./sections/ExperienceSection";
-import educationSection from "./sections/EducationSection";
-import skillsSection from "./sections/SkillsSection";
-import projectsSection from "./sections/ProjectsSection";
-import FooterSection from "./sections/FooterSection";
-import recognitionSection from "./sections/RecognitionSection";
+import React from 'react';
+import './App.css';
+import headerSection from './sections/HeaderSection';
+import experienceSection from './sections/ExperienceSection';
+import educationSection from './sections/EducationSection';
+import skillsSection from './sections/SkillsSection';
+import projectsSection from './sections/ProjectsSection';
+import footerSection from './sections/FooterSection';
+import recognitionSection from './sections/RecognitionSection'
 
-//! This class doesnt really do anything. It takes constructor args and returns set fields
-//! More or less equivalent to (x,y)=>{return {x,y}} and practically equivalent to x => x
+class ContentSection {
+  title: String;
+  body: JSX.Element | JSX.Element[];
 
-// class ContentSection {
-//   title: String;
-//   body: JSX.Element | JSX.Element[];
+  constructor(title: String, body: JSX.Element | JSX.Element[]) {
+    this.title = title; this.body = body;
+  }
+}
 
-//   constructor(title: String, body: JSX.Element | JSX.Element[]) {
-//     this.title = title; this.body = body;
-//   }
-// }
-
-//Turned to an array as a quick rewrite, maybe better as an object
-//using `x-section()` to return JSX is probably a data flow code smell here, esp since you have this data in a json already?
 const contentSections = [
-  ["Experience", experienceSection()],
-  ["Education", educationSection()],
-  ["Skills", skillsSection()],
-  ["Projects", projectsSection()],
-  ["Recognition", recognitionSection()],
-];
+  new ContentSection("Experience", experienceSection()), 
+  new ContentSection("Education", educationSection()),
+  new ContentSection("Skills", skillsSection()),
+  new ContentSection("Projects", projectsSection()),
+  new ContentSection("Recognition", recognitionSection())
+]
 
-function contentSectionElement(
-  title: JSX.Element | JSX.Element[] | string, // my code is WRONG typing (prev was correct), but did it so TS wouldnt yell at me about the mixed typed array
-  body: JSX.Element | JSX.Element[] | string // my code is WRONG typing (prev was correct), but did it so TS wouldnt yell at me about the mixed typed array
-) {
+function contentSectionElement(title: String, body: JSX.Element| JSX.Element[]) {
   return (
-    <div className="content-section">
+    <div className='content-section'>
       <header className="section-header">
         <h2>{title}</h2>
       </header>
       {body}
     </div>
-  );
+  )
 }
 
 function App() {
   return (
     <div className="wrapper">
-      {/* Dont forget about Components!!! */}
-      <HeaderSection />
-
-      {contentSections.map((section) => {
-        console.log(section[1]);
-        return contentSectionElement(section[0], section[1]); //should refactor to component that takes props
-      })}
-
-      {/* Dont forget about Components!!! */}
-      <FooterSection />
+      {headerSection()}
+      {contentSections.map(
+        section => (
+          contentSectionElement(section.title, section.body)
+        )
+      )}
+      {footerSection()}
     </div>
   );
 }
